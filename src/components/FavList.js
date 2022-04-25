32f30644c2605a381c840aa7c1bb5e4ac1ea50d3
Coles Eric <s2200118@BC-C02GR0BJQ6LR-MacBook-Air.local>
2022-04-25 14:15:46 +0300

    first commit

---
+++ src/components/FavList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FavList = () => {
+  return <div>FavList will be header</div>;
+};
+
+export default FavList;
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>home will be here</div>;
+};
+
+export default Home;
